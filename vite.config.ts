d08005fa99e465692599387b11596c425224c05f
Raghvendra sahu <raigenlabs@gmail.com>
2026-06-07 21:55:56 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/mydsajourney/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
